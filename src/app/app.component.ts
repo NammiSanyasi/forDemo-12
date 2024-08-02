cdd3587e5fd0f31e6ba1ff14c0cdb935599ac3f7
NammiSanyasi <nammisanyasi4a@gmail.com>
2024-08-02 22:52:47 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'forDemo';
+  /*products=[ {
+    title: 'samsung s23',
+    price: 250,
+    url: 'https://images.samsung.com/is/image/samsung/levant-galaxy-note10-plus-n975-sm-n975fzsdmid-backauraglow-thumb-182079707?$344_344_PNG',
+  },
+  {
+    title: 'samsung s23',
+  price: 250,
+  url: 'https://images.samsung.com/is/image/samsung/levant-galaxy-note10-plus-n975-sm-n975fzsdmid-backauraglow-thumb-182079707?$344_344_PNG',
+},
+{
+  title: 'samsung s23',
+price: 250,
+url: 'https://images.samsung.com/is/image/samsung/levant-galaxy-note10-plus-n975-sm-n975fzsdmid-backauraglow-thumb-182079707?$344_344_PNG',
+},
+{
+  title: 'samsung s23',
+price: 250,
+url: 'https://images.samsung.com/is/image/samsung/levant-galaxy-note10-plus-n975-sm-n975fzsdmid-backauraglow-thumb-182079707?$344_344_PNG',
+},
+{
+  title: 'samsung s23',
+price: 250,
+url: 'https://images.samsung.com/is/image/samsung/levant-galaxy-note10-plus-n975-sm-n975fzsdmid-backauraglow-thumb-182079707?$344_344_PNG',
+},];*/
+
+products:any[] = [];
+  constructor() {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => res.json())
+      .then((data) => { console.log(data),this.products = data });
+  }
 }
